12ef3f74d1905bf64318ddae584be701da72a50b
sabin ducon <sabinchacko03>
2020-09-24 10:57:23 +0400

    log

--- application/views/material/material_submittal_log.php
+++ application/views/material/material_submittal_log.php
@@ -5,7 +5,7 @@
                 <th>Revision</th>
                 <th>Actual Submittal Date</th>
                 <th>Expected Returned Date</th>
-                <th>Returned Date</th>
+                <th>Actual Returned Date</th>
                 <th>Status</th>
                 <th>Action</th>
             </tr>
@@ -15,7 +15,7 @@
                 <tr>
                     <td><?= $row->revision ?></td>
                     <td><?= date('d-M-Y', strtotime($row->actual_submittal_date)) ?></td>
-                    <td><?= date('d-M-Y', strtotime('+7 days', strtotime($row->actual_submittal_date))) ?></td>
+                    <td><?= date('d-M-Y', strtotime('+14 days', strtotime($row->actual_submittal_date))) ?></td>
                     <td><?= $row->returned_date == '0000-00-00' ? 'NA' : date('d-M-Y', strtotime($row->returned_date)) ?></td>
                     <td><?= $row->status ?></td>
                     <?php if($this->edit_role && $row->status == 'UR'){ ?>
